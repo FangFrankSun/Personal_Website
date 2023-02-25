63324067b10b1ea77bbbc526ddd3727030d4ce37
Frank <115383264+FangFrankSun@users.noreply.github.com>
2023-02-25 03:49:43 -0500

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,49 +1,49 @@
-const STORAGE_KEY = "theme";
-const THEME_ATTR  = "data-theme";
-const QUERY_KEY   = "(prefers-color-scheme: dark)";
+// const STORAGE_KEY = "theme";
+// const THEME_ATTR  = "data-theme";
+// const QUERY_KEY   = "(prefers-color-scheme: dark)";
 
-const themes = {
-  LIGHT: "light",
-  DARK: "dark",
-};
+// const themes = {
+//   LIGHT: "light",
+//   DARK: "dark",
+// };
 
-initTheme();
+// initTheme();
 
-function initTheme() {
-  const savedTheme = localStorage.getItem(STORAGE_KEY);
+// function initTheme() {
+//   const savedTheme = localStorage.getItem(STORAGE_KEY);
 
-  if (savedTheme) {
-    // Storage theme
-    setTheme(savedTheme);
-  } else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
-    // system theme
-    setTheme(themes.DARK);
-  } else {
-    // Default theme
-    setTheme(themes.LIGHT);
-  }
+//   if (savedTheme) {
+//     // Storage theme
+//     setTheme(savedTheme);
+//   } else if (window.matchMedia && window.matchMedia(QUERY_KEY).matches) {
+//     // system theme
+//     setTheme(themes.DARK);
+//   } else {
+//     // Default theme
+//     setTheme(themes.LIGHT);
+//   }
 
-  // Watch for system theme changes
-  window.matchMedia(QUERY_KEY).addEventListener("change", (e) => {
-    const newTheme = e.matches ? themes.DARK : themes.LIGHT;
-    setTheme(newTheme);
-  });
-}
+//   // Watch for system theme changes
+//   window.matchMedia(QUERY_KEY).addEventListener("change", (e) => {
+//     const newTheme = e.matches ? themes.DARK : themes.LIGHT;
+//     setTheme(newTheme);
+//   });
+// }
 
-function toggleTheme() {
-  const theme = getTheme();
-  const newTheme = theme === themes.DARK ? themes.LIGHT : themes.DARK;
-  setTheme(newTheme);
-  localStorage.setItem(STORAGE_KEY, newTheme);
-}
+// function toggleTheme() {
+//   const theme = getTheme();
+//   const newTheme = theme === themes.DARK ? themes.LIGHT : themes.DARK;
+//   setTheme(newTheme);
+//   localStorage.setItem(STORAGE_KEY, newTheme);
+// }
 
-function getTheme() {
-  return document.documentElement.getAttribute(THEME_ATTR);
-}
+// function getTheme() {
+//   return document.documentElement.getAttribute(THEME_ATTR);
+// }
 
-function setTheme(value) {
-  document.documentElement.setAttribute(THEME_ATTR, value);
-}
+// function setTheme(value) {
+//   document.documentElement.setAttribute(THEME_ATTR, value);
+// }
 
 
 
